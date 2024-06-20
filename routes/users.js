e9c4eb8f5e1e4e3d31801be5b0eeee73a043578e
ayowaaab <ayoub.dahmen@esprit.tn>
2024-06-20 10:17:28 -0500

    ✨ added User Model

---
+++ routes/users.js
@@ -0,0 +1,57 @@
+const { User, validate } = require("../models/user");
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const user = await User.find().sort("name");
+  res.send(user);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = new User({ name: req.body.name });
+  user = await user.save();
+
+  res.send(user);
+});
+
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = await User.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name },
+    {
+      new: true,
+    }
+  );
+
+  if (!user)
+    return res.status(404).send("The user with the given ID was not found.");
+
+  res.send(user);
+});
+
+router.delete("/:id", async (req, res) => {
+  const user = await User.findByIdAndRemove(req.params.id);
+
+  if (!user)
+    return res.status(404).send("The user with the given ID was not found.");
+
+  res.send(user);
+});
+
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user)
+    return res.status(404).send("The user with the given ID was not found.");
+
+  res.send(user);
+});
+
+module.exports = router;
