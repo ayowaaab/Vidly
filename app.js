8329e291fcea81d0308960efde8116da47faa391
ayowaaab <ayoub.dahmen@esprit.tn>
2024-06-20 03:51:17 -0500

    ✨ 09-Folder Project V2.0

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 const express = require("express");
+
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
+const Rentals = require("./routes/rentals");
 const app = express();
 const mongoose = require("mongoose");
 
@@ -19,8 +23,7 @@ app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
-
-
+app.use("/api/rentals", Rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
